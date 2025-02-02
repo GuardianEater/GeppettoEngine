7a77b288213aac859ad05b87de3847cc5e7f058e
GuardianEater <2018tcg@gmail.com>
2025-02-01 17:46:11 -0800

    Cleaned up lua it should reflect most things now, tho, strings dont work for some reason

--- Core/ScriptingSystem.cpp
+++ Core/ScriptingSystem.cpp
@@ -19,30 +19,79 @@ namespace Client
 
     ScriptingSystem::ScriptingSystem(Gep::EngineManager& em)
         : ISystem(em)
+        , mLua()
     {
-    }
+        mLua.open_libraries();
 
-    void ScriptingSystem::Initialize()
-    {
-        lua.open_libraries();
+        // TODO: need to make this readonly on the lua side
+        const sol::table log = mLua.create_table("Log");
 
-        lua.new_usertype<glm::vec3>("Vec3",
+        mLua["Log"]["Trace"] = [](const sol::variadic_args& args) 
+        {
+            std::string message;
+            for (auto arg : args)
+                message += arg.get<std::string>();
+
+            Gep::Log::Trace(message);
+        };
+
+        mLua["Log"]["Info"] = [](const sol::variadic_args& args)
+        {
+            std::string message;
+            for (auto arg : args)
+                message += arg.get<std::string>();
+            Gep::Log::Info(message);
+        };
+
+        mLua["Log"]["Warning"] = [](const sol::variadic_args& args)
+        {
+            std::string message;
+            for (auto arg : args)
+                message += arg.get<std::string>();
+            Gep::Log::Warning(message);
+        };
+
+        mLua["Log"]["Error"] = [](const sol::variadic_args& args)
+        {
+            std::stringstream ss;
+            for (auto&& arg : args) {
+                if (arg.is<std::string>()) ss << arg.as<std::string>();
+                else if (arg.is<int>())    ss << arg.as<int>();
+                else if (arg.is<float>())  ss << arg.as<float>();
+                else if (arg.is<double>()) ss << arg.as<double>();
+                else if (arg.is<bool>())   ss << arg.as<bool>();
+                else if (arg.is<glm::vec3>()) {
+                    glm::vec3 vec = arg.as<glm::vec3>();
+                    ss << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+                }
+                else {
+                    ss << "???";
+                }
+            }
+
+            Gep::Log::Error(ss.str());
+        };
+
+        mLua.new_usertype<glm::vec3>("vec3",
             "x", &glm::vec3::x,
             "y", &glm::vec3::y,
             "z", &glm::vec3::z
         );
+    }
 
-        lua.new_usertype<Client::Transform>("Transform",
-            "position", &Transform::position,
-            "scale", &Transform::scale,
-            "rotation", &Transform::rotation
-        );
+    void ScriptingSystem::Initialize()
+    {
+        //lua.new_usertype<Client::Transform>("Transform",
+        //    "position", &Transform::position,
+        //    "scale", &Transform::scale,
+        //    "rotation", &Transform::rotation
+        //);
 
-        lua.new_usertype<Client::RigidBody>("RigidBody",
-            "velocity", &RigidBody::velocity,
-            "acceleration", &RigidBody::acceleration,
-            "rotational", &RigidBody::rotationalVelocity
-        );
+        //lua.new_usertype<Client::RigidBody>("RigidBody",
+        //    "velocity", &RigidBody::velocity,
+        //    "acceleration", &RigidBody::acceleration,
+        //    "rotational", &RigidBody::rotationalVelocity
+        //);
     }
 
     void ScriptingSystem::Update(float dt)
@@ -50,18 +99,32 @@ namespace Client
         std::vector<Gep::Entity>& entities = mManager.GetEntities<Script>();
         for (Gep::Entity entity : entities)
         {
+            sol::table entityTable = mLua.create_table();
+            sol::environment entityEnvironment(mLua, sol::create, mLua.globals());
+
             Script& script = mManager.GetComponent<Client::Script>(entity);
 
-            if (mManager.HasComponent<Transform>(entity))
+            mManager.ForEachComponent(entity, [&](const Gep::ComponentData& data) 
             {
-                lua["Transform"] = &mManager.GetComponent<Transform>(entity);
-            }
-            if (mManager.HasComponent<RigidBody>(entity))
-            {
-                lua["RigidBody"] = &mManager.GetComponent<RigidBody>(entity);
-            }
+                mSetComponentMemberReferences[data.index](entity, entityTable);
+            });
 
-            lua.script(script.data, sol::script_pass_on_error);
+            entityEnvironment["self"] = entityTable;
+
+            static std::string lastError;
+            sol::protected_function_result result = mLua.script(script.data, entityEnvironment, sol::script_pass_on_error);
+
+            // prints only the lastest error
+            if (!result.valid())
+            {
+                sol::error err = result;
+
+                if (err.what() != lastError)
+                {
+                    lastError = err.what();
+                    Gep::Log::Error("Error in script on entity [", entity, "]: ", err.what());
+                }
+            }
         }
     }
 }
--- Core/ScriptingSystem.hpp
+++ Core/ScriptingSystem.hpp
@@ -12,6 +12,8 @@
 #include <Script.hpp>
 #include "EngineManager.hpp"
 
+#include <sol/sol.hpp>
+
 namespace Client
 {
     class ScriptingSystem : public Gep::ISystem
@@ -19,9 +21,45 @@ namespace Client
     public:
         ScriptingSystem(Gep::EngineManager& em);
 
+        template <typename... ComponentTypes>
+        void OnComponentsRegistered(Gep::type_list<ComponentTypes...> componentTypes);
+
         void Initialize();
         void Update(float dt);
+
+    private:
+        // given a componentIndex, sets up the lua fields for that compoennt
+        std::vector<std::function<void(Gep::Entity, sol::table&)>> mSetComponentMemberReferences;
+        sol::state mLua;
+        sol::environment mEnvironment;
     };
+
+    template<typename ...ComponentTypes>
+    inline void ScriptingSystem::OnComponentsRegistered(Gep::type_list<ComponentTypes...> componentTypes)
+    {
+        componentTypes.for_each([&]<typename ComponentType>()
+        {
+            mSetComponentMemberReferences.push_back([&](Gep::Entity entity, sol::table& entityTable)
+            {
+                ComponentType& component = mManager.GetComponent<ComponentType>(entity);
+                std::string typeName = Gep::GetTypeInfo<ComponentType>().PrettyName();
+
+                auto view = rfl::to_view(component);
+
+                sol::table componentTable = mLua.create_table();
+                view.apply([&](const auto& field)
+                {
+                    using FieldType = std::remove_reference_t<decltype(*field.value())>;
+                    const std::string fieldName(field.name());
+                    auto& fieldValue = *field.value();
+
+                    componentTable[fieldName] = field.value();
+                });
+
+                entityTable[typeName] = componentTable;
+            });
+        });
+    }
 }
 
 
