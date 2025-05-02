fe6d135696325adb16cc581adaec5934667d9657
GuardianEater <2018tcg@gmail.com>
2025-05-01 20:50:32 -0700

    Archetypes on colliders!

--- Core/CollisionSystem.cpp
+++ Core/CollisionSystem.cpp
@@ -22,73 +22,95 @@ namespace Client
     {
     }
 
+    struct SphereEntity
+    {
+        float xMin{};
+        float xMax{};
+        float radius{};
+        Gep::Entity entity;
+        Transform* transform;
+        SphereCollider* collider;
+    };
 
+    struct CubeEntity
+    {
+        Gep::Entity entity;
+        Transform* transform;
+        CubeCollider* collider;
+    };
 
     void CollisionSystem::Update(float dt)
     {
-        const std::vector<Gep::Entity>& cubeEntities = mManager.GetEntities<Transform, CubeCollider>();
-        const std::vector<Gep::Entity>& sphereEntities = mManager.GetEntities<Transform, SphereCollider>();
+        std::vector<SphereEntity> sphereEntities;
+
+        mManager.ForEachArchetype<Transform, SphereCollider>([&](Gep::Entity entity, Transform& t, SphereCollider& s)
+        {
+            float radius = std::max({ t.scale.x, t.scale.y, t.scale.z }) / 2.0f;
+            float minX = t.position.x - radius;
+            float maxX = t.position.x + radius;
+
+            sphereEntities.push_back({ minX, maxX, radius, entity, &t, &s });
+        });
+
+        std::sort(sphereEntities.begin(), sphereEntities.end(), [](const auto& a, const auto& b) 
+        {
+            return a.xMin < b.xMin;
+        });
 
         // Sphere-Sphere collisions
         for (size_t i = 0; i < sphereEntities.size(); ++i)
         {
-            Gep::Entity entity = sphereEntities[i];
-            Transform& transform = mManager.GetComponent<Transform>(entity);
-            SphereCollider& collider = mManager.GetComponent<SphereCollider>(entity);
-
             for (size_t j = i + 1; j < sphereEntities.size(); ++j)
             {
-                Gep::Entity otherEntity = sphereEntities[j];
-                Transform& otherTransform = mManager.GetComponent<Transform>(otherEntity);
-                SphereCollider& otherCollider = mManager.GetComponent<SphereCollider>(otherEntity);
+                if (sphereEntities[j].xMin > sphereEntities[i].xMax)
+                    break;
 
-                if (Gep::SphereSphere(Gep::Sphere{ transform.position, std::max({transform.scale.x, transform.scale.y, transform.scale.z}) / 2.0f },
-                    Gep::Sphere{ otherTransform.position, std::max({otherTransform.scale.x, otherTransform.scale.y, otherTransform.scale.z}) / 2.0f }))
+                SphereEntity& view0 = sphereEntities[i];
+                SphereEntity& view1 = sphereEntities[j];
+
+                if (Gep::SphereSphere({ view0.transform->position, view0.radius },
+                                      { view1.transform->position, view1.radius }))
                 {
-                    // collision detected
-                    Gep::Log::Info("Collision detected between entity ", entity, " and entity ", otherEntity);
+                    Gep::Log::Info("Collision detected between entity ", view0.entity, " and entity ", view1.entity);
                 }
             }
         }
 
+        std::vector<CubeEntity> cubeEntities;
+
+        mManager.ForEachArchetype<Transform, CubeCollider>([&](Gep::Entity entity, Transform& t, CubeCollider& c)
+        {
+            cubeEntities.push_back({ entity, &t, &c });
+        });
+
         // Cube-Cube collisions
         for (size_t i = 0; i < cubeEntities.size(); ++i)
         {
-            Gep::Entity entity = cubeEntities[i];
-            Transform& transform = mManager.GetComponent<Transform>(entity);
-            CubeCollider& collider = mManager.GetComponent<CubeCollider>(entity);
-
             for (size_t j = i + 1; j < cubeEntities.size(); ++j)
             {
-                Gep::Entity otherEntity = cubeEntities[j];
-                Transform& otherTransform = mManager.GetComponent<Transform>(otherEntity);
-                CubeCollider& otherCollider = mManager.GetComponent<CubeCollider>(otherEntity);
+                const CubeEntity& view0 = cubeEntities[i];
+                const CubeEntity& view1 = cubeEntities[j];
 
-                if (Gep::CubeCube(Gep::Cube{ transform.position, transform.scale / 2.0f, transform.rotation },
-                    Gep::Cube{ otherTransform.position, otherTransform.scale / 2.0f, otherTransform.rotation }))
+
+                if (Gep::CubeCube({ view0.transform->position, view0.transform->scale * 0.5f, view0.transform->rotation },
+                                  { view1.transform->position, view1.transform->scale * 0.5f, view1.transform->rotation }))
                 {
                     // collision detected
-                    Gep::Log::Info("Collision detected between entity ", entity, " and entity ", otherEntity);
+                    Gep::Log::Info("Collision detected between entity ", view0.entity, " and entity ", view1.entity);
                 }
             }
         }
 
         // Sphere-Cube collisions
-        for (Gep::Entity entity : sphereEntities)
+        for (const CubeEntity& view0 : cubeEntities)
         {
-            Transform& transform = mManager.GetComponent<Transform>(entity);
-            SphereCollider& collider = mManager.GetComponent<SphereCollider>(entity);
-
-            for (Gep::Entity otherEntity : cubeEntities)
+            for (const SphereEntity& view1 : sphereEntities)
             {
-                Transform& otherTransform = mManager.GetComponent<Transform>(otherEntity);
-                CubeCollider& otherCollider = mManager.GetComponent<CubeCollider>(otherEntity);
-
-                if (Gep::CubeSphere(Gep::Cube{ otherTransform.position, otherTransform.scale / 2.0f, otherTransform.rotation },
-                    Gep::Sphere{ transform.position, std::max({transform.scale.x, transform.scale.y, transform.scale.z}) / 2.0f }))
+                if (Gep::CubeSphere({view0.transform->position, view0.transform->scale * 0.5f, view0.transform->rotation },
+                                    { view1.transform->position, view1.radius }))
                 {
                     // collision detected
-                    Gep::Log::Info("Collision detected between entity ", entity, " and entity ", otherEntity);
+                    Gep::Log::Info("Collision detected between entity ", view0.entity, " and entity ", view1.entity);
                 }
             }
         }
